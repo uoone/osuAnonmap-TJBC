761f5b35a119a1cda571e3bb4a2c41099a5fbc3b
uone <uone0001516659@yahoo.co.jp>
2024-08-18 05:04:25 +0900

    fix unused code for single osz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import zipfile
 import os
+import re
 import sys
 import shutil
 import json
@@ -39,15 +40,12 @@ config['taiko'] = str_to_bool(config['taiko'])
 
 HOME = os.getcwd()
 maps = os.path.join(HOME, file_name)
-unzipped_maps = os.path.join(HOME, 'unzipped_maps')
 extracted_oszs = os.path.join(HOME, 'extracted_oszs')
 to_osz = os.path.join(HOME, 'to_osz')
 outdir = os.path.join(HOME, 'output')
 
 if os.path.exists(outdir):
     shutil.rmtree(outdir)
-if os.path.exists(unzipped_maps):
-    shutil.rmtree(unzipped_maps)
 if os.path.exists(extracted_oszs):
     shutil.rmtree(extracted_oszs)
 if os.path.exists(to_osz):
@@ -55,7 +53,6 @@ if os.path.exists(to_osz):
 if os.path.exists('./anonmaps.zip'):
     os.remove('./anonmaps.zip')
 
-os.mkdir(unzipped_maps)
 os.mkdir(extracted_oszs)
 os.mkdir(to_osz)
 
@@ -63,6 +60,7 @@ with zipfile.ZipFile(maps, "r") as zip_ref:
     zip_ref.extractall(extracted_oszs)
 
 osz_folders = os.listdir(extracted_oszs)
+print(osz_folders)
 title_list = []
 duplicated_title_list = []
 print('Anonymizing .osz files')
@@ -76,6 +74,7 @@ artist = ''
 creator = ''
 version = ''
 for file in osu_list:
+    print(osu_list)
     to_write = []
     with open(os.path.join(extracted_oszs, file), encoding='utf8') as f:
         lines = [line.rstrip() for line in f]
@@ -106,23 +105,23 @@ for file in osu_list:
             repl = '256,192,' + ','.join(hit_objects[2:4]) + ',' + str(hit_sound) + ',' + ','.join(hit_objects[5:])
         if repl:
             to_write.append(repl+'\n')
-        else:
-            to_write.append(line+'\n')
 
+    artist = re.sub(r'[\\/:*?"<>|]+','', artist)
+    title = re.sub(r'[\\/:*?"<>|]+','', title)
+    creator = re.sub(r'[\\/:*?"<>|]+','', creator)
+    version = re.sub(r'[\\/:*?"<>|]+','', version)
     osu_to_save = os.path.join(extracted_oszs, f'{artist} - {title} ({creator}) [{version}].osu')
 
     f = open(osu_to_save, "a", encoding='utf8')
     f.writelines(to_write)
     f.close()
 
-to_osz_folder = os.path.join(extracted_oszs)
-move_file_list = glob.glob(to_osz_folder + "/*")
-for item in move_file_list:
-    shutil.move(item, to_osz)
+copy_file_list = glob.glob(os.path.join(extracted_oszs) + "/*")
+for item in copy_file_list:
+    shutil.copy(item, to_osz)
 
 zip_folder(to_osz, os.path.join(HOME, 'anonmaps.zip'))
 
-shutil.rmtree(unzipped_maps)
 shutil.rmtree(to_osz)
 shutil.rmtree(extracted_oszs)
 
